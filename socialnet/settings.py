9162846d6acdf0821ca2cb429bf0cc776dc46fb6
adrian-y1 <adrian.yonan1@gmail.com>
2022-07-03 19:35:20 +1000

    edited settings

--- socialnet/settings.py
+++ socialnet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SOCIALFRONT_DJANGO_S_K')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://socialfront50.herokuapp.com/']
+ALLOWED_HOSTS = ['https://socialfront50.herokuapp.com/', 'localhost']
 
 
 # Application definition
