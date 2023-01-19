186460450642ec132aae45e462c323cc80cf0d96
adrian-y1 <adrian.yonan1@gmail.com>
2023-01-19 14:47:05 +1100

    Updated: Settings.py

--- socialnet/settings.py
+++ socialnet/settings.py
@@ -29,6 +29,8 @@ DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['web-production-1fd4.up.railway.app', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['web-production-1fd4.up.railway.app/*',]
+
 # Application definition
 
 INSTALLED_APPS = [
